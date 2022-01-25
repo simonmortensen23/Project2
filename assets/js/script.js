2803a438c46943ceab199215efacdac31441b153
simonmortensen23 <simon020393@gmail.com>
2022-01-25 12:10:53 +0000

    Style quiz area

---
+++ assets/js/script.js
@@ -0,0 +1,10 @@
+function startGame(){
+    if(register.username.value){
+        /* var UI = document.getElementById(username).value; */ 
+        const newDiv = document.getElementById(user);
+        newDiv,innerHTML = `Welcome`;
+    }
+    else{
+        alert("No username");
+    }   
+}
